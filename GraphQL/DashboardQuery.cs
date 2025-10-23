06cd35b3a425fa9a930fb311a496dadb525f0e08
Julius Franklin Asante-Ayeh <juliusfranklin24@gmail.com>
2025-10-23 11:55:40 +0000

    Add dashboard and maual queries classes

---
+++ GraphQL/DashboardQuery.cs
@@ -0,0 +1,13 @@
+﻿using attendance_tracking_backend.Data;
+using HotChocolate.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace attendance_tracking_backend.GraphQL
+{
+    [ExtendObjectType(OperationTypeNames.Query)]
+    public class DashboardQuery
+    {
+
+    }
+}
