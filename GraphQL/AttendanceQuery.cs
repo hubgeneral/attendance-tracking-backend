3106e0225f135c3d7014961481964a8be8ea347b
Julius Franklin Asante-Ayeh <juliusfranklin24@gmail.com>
2025-10-29 17:43:38 +0000

    fetch attendance by date range

--- GraphQL/AttendanceQuery.cs
+++ GraphQL/AttendanceQuery.cs
@@ -2,6 +2,7 @@
 using HotChocolate.Data;
 using attendance_tracking_backend.Data;
 using Microsoft.EntityFrameworkCore;
+using attendance_tracking_backend.Helpers;
 
 
 namespace attendance_tracking_backend.GraphQL
@@ -22,6 +23,34 @@ namespace attendance_tracking_backend.GraphQL
             return dbcontext.Attendances.Where(a => a.AppUserId == user!.Id);
         }
 
+
+        public IQueryable<Attendance> GetAttendanceByDate(DateTime startDate, DateTime stopDate, [Service] DatabaseContext dbcontext) {
+
+            // Normalize dates: ensure we compare only dates, not time
+            var start = startDate.Date;
+            var end = stopDate.Date;
+
+            // Query Attendances within the specified date range
+            var results = dbcontext.Attendances
+                .Where(a => a.CurrentDate >= DateOnly.FromDateTime(start)
+                         && a.CurrentDate <= DateOnly.FromDateTime(end))
+                .Select(a => new Attendance
+                {
+                    Id = a.Id,
+                    ClockIn = a.ClockIn,
+                    ClockOut = a.ClockOut,
+                    ClockingType = a.ClockingType,
+                    Status = a.Status,
+                    CurrentDate = a.CurrentDate,
+                    AppUserId = a.AppUserId,
+                    User = a.User,
+                    EntryExitLogs = a.EntryExitLogs,
+                    TotalHoursWorked = CalculateWorkHours.CalculateWorkingHours(a.ClockIn, a.ClockOut)
+                });
+
+            return results;
+
+        }
         
     }
 }
---
+++ Helpers/CalculateWorkHours.cs
@@ -0,0 +1,24 @@
+﻿namespace attendance_tracking_backend.Helpers
+{
+    public class CalculateWorkHours
+    {
+        public static decimal CalculateWorkingHours(DateTime? clockIn, DateTime? clockOut)
+        {
+            if (clockIn == null || clockOut == null)
+                return 0;
+
+            var startWork = new TimeSpan(8, 0, 0);
+            var endWork = new TimeSpan(17, 0, 0);
+
+            var actualStart = clockIn.Value.TimeOfDay < startWork ? startWork : clockIn.Value.TimeOfDay;
+            var actualEnd = clockOut.Value.TimeOfDay > endWork ? endWork : clockOut.Value.TimeOfDay;
+
+            var total = actualEnd - actualStart;
+
+            if (total.TotalHours < 0)
+                return 0;
+
+            return (decimal)total.TotalHours;
+        }
+    }
+}
