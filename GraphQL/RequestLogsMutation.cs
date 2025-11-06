d226d2b31b01842aa6c117b905512adbbc228cea
Julius Franklin Asante-Ayeh <juliusfranklin24@gmail.com>
2025-11-06 09:42:11 +0000

    add requestlogs and manuallogs changes

--- GraphQL/RequestLogsMutation.cs
+++ GraphQL/RequestLogsMutation.cs
@@ -7,7 +7,7 @@ namespace attendance_tracking_backend.GraphQL
     [ExtendObjectType(OperationTypeNames.Mutation)]
     public class RequestLogsMutation
     {
-        public async Task<string> CreateRequest(int userid, string reason ,[Service]DatabaseContext dbcontext)
+        public async Task<string> CreateRequestLog(int userid, string reason ,[Service]DatabaseContext dbcontext)
         {
             var user = await dbcontext.Users.FindAsync(userid);
             if (user == null) throw new GraphQLException("User does not exits");
@@ -19,8 +19,7 @@ namespace attendance_tracking_backend.GraphQL
                 EmployeeName = user.EmployeeName,
                 Reason = reason,
                 TimeOfDay = currentTime,
-                AppUserId = user.Id,
-                
+                AppUserId = user.Id,              
             };
             dbcontext.RequestLogs.Add(newRequest);
             await dbcontext.SaveChangesAsync();
@@ -28,14 +27,13 @@ namespace attendance_tracking_backend.GraphQL
             return "New request created";
         }
 
-        public async Task<string> UpdateRequest(int requestId,string reason, [Service] DatabaseContext dbcontext)
+        public async Task<string> UpdateRequestLog(int requestId,string reason, [Service] DatabaseContext dbcontext)
         {         
             var request = await dbcontext.RequestLogs.FindAsync(requestId);
             if (request == null) throw new GraphQLException("Request record does not exist");
 
             request.Reason = reason;
-            await dbcontext.SaveChangesAsync();  
-            
+            await dbcontext.SaveChangesAsync();              
             return "Request record updated";
         }
         
@@ -45,7 +43,6 @@ namespace attendance_tracking_backend.GraphQL
             if (request == null) throw new GraphQLException("Request record does not exist");
             dbcontext.Remove(request);
             await dbcontext.SaveChangesAsync();
-
             return "Request record deleted successfully";
         }
     }
